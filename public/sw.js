a8c48f157ef6a82aaff56fa90eadec8f6a468fde
theroasters84-wq <theroasters84@gmail.com>
2026-01-09 01:16:09 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,11 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-    // Απαραίτητο για να περάσει το τεστ του Chrome
     event.respondWith(fetch(event.request));
 });

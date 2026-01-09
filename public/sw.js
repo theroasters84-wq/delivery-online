dd85f7bb267aa384e68b7a27bebf9ac1f4817904
theroasters84-wq <theroasters84@gmail.com>
2026-01-09 02:11:56 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,23 @@
+const CACHE_NAME = 'roasters-v1';
+const ASSETS = [
+  '/driver.html',
+  '/manifest-driver.json',
+  'https://img.icons8.com/color/512/motorcycle.png'
+];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });

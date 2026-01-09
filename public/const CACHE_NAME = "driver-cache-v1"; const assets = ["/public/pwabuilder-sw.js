6531957c9c335c17126e50b4186a1057d6c03809
theroasters84-wq <theroasters84@gmail.com>
2026-01-09 17:55:55 +0200

    Create pwabuilder-sw.js

---
+++ public/const CACHE_NAME = "driver-cache-v1"; const assets = ["/public/pwabuilder-sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "driver-cache-v1";
+const assets = ["/driver.html", "/manifest-driver.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});

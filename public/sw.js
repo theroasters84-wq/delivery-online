77d5bd8479c84d6c61d9b3c3aec5c78c0fce54b3
theroasters84-wq <theroasters84@gmail.com>
2026-01-09 02:42:09 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,14 @@
-const CACHE_NAME = 'roasters-v1';
-const ASSETS = [
-  '/driver.html',
-  '/manifest-driver.json',
-  'https://img.icons8.com/color/512/motorcycle.png'
-];
+const CACHE_NAME = 'roasters-cache-v1';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+    // Απλή μεταφορά αιτημάτων για να δουλεύει το PWA
+    event.respondWith(fetch(event.request));
 });
